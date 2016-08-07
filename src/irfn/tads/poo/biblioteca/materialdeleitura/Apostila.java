21d6cecf6d8548cf9300b509d605e09fd66d8f02
albert06 <alberts258456@hotmail.com>
2016-08-07 16:38:30 -0300

    resto do outro commit

--- src/irfn/tads/poo/biblioteca/materialdeleitura/Apostila.java
+++ src/irfn/tads/poo/biblioteca/materialdeleitura/Apostila.java
@@ -5,10 +5,11 @@ package irfn.tads.poo.biblioteca.materialdeleitura;
 public class Apostila extends ItemAcervo{
 
 	
-	public Apostila(String titulo, String autor, int cdg, double custo) {
+	public Apostila(String titulo, String autor, int cdg, double custo, double multa) {
 		this.titulo = titulo;
 		this.autor = autor;		
 		this.custo = custo;
 		this.codigoItem = cdg;
+		this.multa = multa;
 	}	
 }
